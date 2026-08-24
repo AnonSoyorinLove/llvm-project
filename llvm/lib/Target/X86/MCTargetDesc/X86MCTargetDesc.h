5db9e313c5304d28a0fb41c0ce82e078bef06fef
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-08-24 18:02:07 +0800

    llvm: fix integer type header dependencies
    Change-Id: I04036efa3dcb8c90c685ccbfeef6124e20737bf7

--- llvm/lib/Target/X86/MCTargetDesc/X86MCTargetDesc.h
+++ llvm/lib/Target/X86/MCTargetDesc/X86MCTargetDesc.h
@@ -13,6 +13,8 @@
 #ifndef LLVM_LIB_TARGET_X86_MCTARGETDESC_X86MCTARGETDESC_H
 #define LLVM_LIB_TARGET_X86_MCTARGETDESC_X86MCTARGETDESC_H
 
+#include <cstdint>
+
 #include <memory>
 #include <string>
 
